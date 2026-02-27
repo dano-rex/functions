3621b06fee1200acbf20cbb271422d2936ddd5b0
dano-rex <danorex22@gmail.com>
2026-02-27 22:33:44 +0100

    Implement multiply function for three integers
    Added a function to multiply three integers.

--- mul.cpp
+++ mul.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int multiply (int a, int b,int c) {
+  int result = a * b * c;
+  
+  return result;
+}
+
+int main() {
+ multiply (a,b,c);
+  return 0;
+}
