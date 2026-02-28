f235b976d22967c33c42a63e193c874e38d0f6b0
dano-rex <danorex22@gmail.com>
2026-02-28 15:19:58 +0100

    Implement function to display multiples of 10
    Added a function to print multiples of 10 from 10 to 100.

--- multiplies_of_10.py
+++ multiplies_of_10.py
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int multiply(){
+  
+  int counter;
+  int result = 0;
+  
+  for (counter = 10; counter<=100; counter = counter + 10)
+  {
+    cout <<counter <<" ";
+    
+  }
+  
+  return result;
+
+}
+int main() {
+  multiply();
+
+  return 0;
+}
